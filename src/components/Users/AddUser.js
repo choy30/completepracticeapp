3b2375d05bc1a7dd603fa521cb61500df7a423b3
devYeonjun <alcls0026@gmail.com>
2022-05-16 23:01:14 +1200

    Manage list of users via state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -17,7 +17,8 @@ const AddUser = (props) => {
 			return;
 		}
 
-		console.log(enteredUsername, enteredAge);
+		props.onAddUser(enteredUsername, enteredAge);
+
 		setEnteredUsername("");
 		setEnteredAge("");
 	};
